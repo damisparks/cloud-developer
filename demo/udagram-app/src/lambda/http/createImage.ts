9dce703cc3c6916e4605d2d1bf598be7d0bddd94
Damilola Omifare <omifaredammy@gmail.com>
2020-10-28 23:29:57 +0000

    refactor : Add serverless and image create feature.

--- demo/udagram-app/src/lambda/http/createImage.ts
+++ demo/udagram-app/src/lambda/http/createImage.ts
@@ -5,23 +5,30 @@ import * as uuid from 'uuid'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 const imagesTable = process.env.IMAGES_TABLE
+const groupsTable = process.env.GROUPS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent):Promise<APIGatewayProxyResult> => {
     console.log('Processing event: ', event)
-    const itemId = uuid.v4()
-  
-    const parsedBody = JSON.parse(event.body)
-  
-    const newItem = {
-      id: itemId,
-      ...parsedBody
+
+    const groupId = event.pathParameters.groupId
+    const validGroupId = await groupExists(groupId)
+
+
+    if(!validGroupId){
+      return {
+        statusCode: 404,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify({
+          erorr: 'Group does not exist',
+        }),
+      }
     }
-  
-    await docClient.put({
-      TableName: imagesTable,
-      Item: newItem
-    }).promise()
-  
+
+    const imageId = uuid.v4()
+    const newItem = createImage(groupId, imageId, event)
+    
     return {
       statusCode: 201,
       headers: {
@@ -30,5 +37,40 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       body: JSON.stringify({
         newItem
       })
+    }
+
+
+    async function groupExists(groupId: string) {
+      const result = await docClient
+        .get({
+          TableName: groupsTable,
+          Key: {
+            id: groupId,
+          },
+        })
+        .promise()
+  
+        console.log(' Get Group: ', result)
+        return !!result.Item
+    }
+
+    async function createImage(groupId: string, imageId: string, event: APIGatewayProxyEvent) {
+      const timestamp = new Date().toISOString()
+      const newImage = JSON.parse(event.body)
+
+      const newItem = {
+        groupId,
+        timestamp,  
+        imageId,
+        ...newImage
+      }
+      console.log('Storing new item : ', newItem)
+
+      await docClient.put({
+        TableName: imagesTable,
+        Item: newItem
+      }).promise()
+
+      return newItem
     }
   }
