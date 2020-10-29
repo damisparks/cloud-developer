995ff44c227442c6664838f8ab81e5aa389d3d6c
Damilola Omifare <omifaredammy@gmail.com>
2020-10-29 23:10:49 +0000

    feat : Add signed URL  feature.

--- demo/udagram-app/src/lambda/http/createImage.ts
+++ demo/udagram-app/src/lambda/http/createImage.ts
@@ -4,8 +4,16 @@ import * as AWS from 'aws-sdk'
 import * as uuid from 'uuid'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
+
+// ADD AN INSTANCE OF S3 
+const s3 = new AWS.S3({
+  signatureVersion: 'v4'
+})
+
 const imagesTable = process.env.IMAGES_TABLE
 const groupsTable = process.env.GROUPS_TABLE
+const bucketName = process.env.IMAGE_S3_BUCKET
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent):Promise<APIGatewayProxyResult> => {
     console.log('Processing event: ', event)
@@ -27,7 +35,9 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
     }
 
     const imageId = uuid.v4()
-    const newItem = createImage(groupId, imageId, event)
+    const newItem = await createImage(groupId, imageId, event)
+
+    const url = getUploadUrl(imageId)
     
     return {
       statusCode: 201,
@@ -35,10 +45,10 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
         'Access-Control-Allow-Origin': '*'
       },
       body: JSON.stringify({
-        newItem
+        newItem: newItem,
+        uploadUrl: url
       })
     }
-
 
     async function groupExists(groupId: string) {
       const result = await docClient
@@ -54,7 +64,7 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
         return !!result.Item
     }
 
-    async function createImage(groupId: string, imageId: string, event: APIGatewayProxyEvent) {
+    async function createImage(groupId: string, imageId: string, event: any) {
       const timestamp = new Date().toISOString()
       const newImage = JSON.parse(event.body)
 
@@ -62,7 +72,8 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
         groupId,
         timestamp,  
         imageId,
-        ...newImage
+        ...newImage,
+        imageUrl : `https://${bucketName}.s3.amazonaws.com/${imageId}`
       }
       console.log('Storing new item : ', newItem)
 
@@ -72,5 +83,13 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       }).promise()
 
       return newItem
+    }
+
+    function getUploadUrl(imageId: string) {
+      return s3.getSignedUrl('putObject', {
+        Bucket: bucketName, 
+        Key: imageId, 
+        Expires: parseInt(urlExpiration)
+      }) 
     }
   }
