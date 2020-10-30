41b512f89e715acf9d2312ff5486d7de9aad3de5
Damilola Omifare <omifaredammy@gmail.com>
2020-10-30 22:21:04 +0000

    feat : Add presigned URL

--- demo/client/src/config.ts
+++ demo/client/src/config.ts
@@ -1,4 +1,5 @@
-export const apiEndpoint = '...'
+const apiId = '4wss9zqdd6'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   domain: '...',
