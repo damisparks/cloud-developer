9294188698317ae1ef440c6e68859ba6d0cd73a0
Damilola Omifare <omifaredammy@gmail.com>
2020-11-01 20:13:18 +0000

    feat : Add clientId & domain values.

--- demo/client/src/config.ts
+++ demo/client/src/config.ts
@@ -2,7 +2,8 @@ const apiId = '4wss9zqdd6'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
+  domain: 'damisparks.eu.auth0.com',
+  // clientId: 'XSsCMx9ctSnTdxmhKxX6xlR1kpUgwo2Y',
+  clientId: 'JEsJUxHtpM57NiKlgnhqvFR8YRKW6ItU',
   callbackUrl: 'http://localhost:3000/callback'
 }
