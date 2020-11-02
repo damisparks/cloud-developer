d55d1407c49c7e2bbef75343394d881ec2f4d7c8
Damilola Omifare <omifaredammy@gmail.com>
2020-11-02 21:48:18 +0000

    feat : Add Create Todo function - Add create todo function. - Add serverless aws documentation plugin for api documentation. - Add todo item

--- project-4/serverless-todo-app/backend/src/lambda/http/createTodo.ts
+++ project-4/serverless-todo-app/backend/src/lambda/http/createTodo.ts
@@ -1,12 +1,41 @@
 import 'source-map-support/register'
-
+import * as uuid from 'uuid'
+import * as AWS from 'aws-sdk'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import { TodoItem } from '../../models/TodoItem'
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  //  Implement creating a new TODO item
+  const todoId = uuid.v4()
+  const newTodoItem: CreateTodoRequest = JSON.parse(event.body)
+  console.log('Processing newTodoItem: ', newTodoItem)
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+  const item: TodoItem = {
+    todoId : todoId,
+    userId: 'fakeuserid',
+    name: newTodoItem.name,
+    dueDate: newTodoItem.dueDate,
+    done: false,
+    createdAt: new Date().toISOString()
+    // ...newTodoItem
+  }
+
+  await docClient.put({
+    TableName: todosTable,
+    Item: item
+  }).promise()
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      item
+    })
+  }
 }
