fe392a54fba1e6c350a7c2394b767166698f3851
Damilola Omifare <omifaredammy@gmail.com>
2020-11-22 22:21:56 +0000

    feat: Update codebase for Udacity submission.

--- project-6/capstone/project-dami-backend/src/lambda/http/getTodos.ts
+++ project-6/capstone/project-dami-backend/src/lambda/http/getTodos.ts
@@ -1,32 +1,42 @@
 import {
   APIGatewayProxyEvent,
   APIGatewayProxyResult,
-  APIGatewayProxyHandler
-} from "aws-lambda"
-import "source-map-support/register"
+  APIGatewayProxyHandler,
+} from 'aws-lambda'
+import 'source-map-support/register'
 
-import { getAllTodos } from "../../businessLogic/todos"
-import { createLogger } from "../../utils/logger"
-import { getUserId } from "../utils"
+import { getAllTodos } from '../../businessLogic/todos'
+import { createLogger } from '../../utils/logger'
+import { getUserId } from '../utils'
 
-const logger = createLogger("getTodos")
+const logger = createLogger('getTodos')
 
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  logger.info("Processing event: ", event)
+  logger.info('Processing event: ', event)
 
-  // TODO: Get all TODO items for a current user
+  // Get all TODO items for a current user
   const userId = getUserId(event)
   const items = await getAllTodos(userId)
 
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*"
-    },
-    body: JSON.stringify({
-      items
-    })
+  if (!items) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: 'Todo does not exits.',
+    }
+  } else {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        items,
+      }),
+    }
   }
 }
